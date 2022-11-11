90f8519f2a96d0deef8f4a744e2740e5b978895f
apple <apple@apples-MacBook-Pro.local>
2022-11-11 22:47:17 +0900

    [FIX] modeling & utils 기능

--- api/utils/utils.py
+++ api/utils/utils.py
@@ -5,18 +5,18 @@ from api.models import *
 
 def preprocess_data(datas):
     phase1 = datas.strip()
-    phase2 = re.split("[.?!]",phase1)
-    result = [re.sub("[^\w|가-힣+?!.,\s]", "", i).strip() for i in phase2 if len(i)>0]
+    phase2 = re.compile(r'([^\.!?]*[\.!?])', re.M)
+    phase3 = phase2.findall(phase1)
+    result = [re.sub("[^\w|가-힣+?!.,\s]", "", i).strip() for i in phase3 if len(i)>0]
     return result
 
-def make_project_savepoint_obj():
+def make_project_obj():
     project = Project.objects.create(
-        project_title="프로젝트" #프로젝트/현재시간/유저이름
+        project_title="프로젝트", #프로젝트/현재시간/유저이름
+        savedpoint=""
     )
-    SavePoint.objects.create(
-        project=project,
-        savepoint=f"../savepoint/{project.project_title}{project.id}/"    
-    )
+    project.savedpoint=f"../savepoint/{project.project_title}{project.id}/"
+    project.save()
     return project.id
     
 def make_text_obj(project_id, complete_preprocess):
@@ -27,17 +27,12 @@ def make_text_obj(project_id, complete_preprocess):
             index=i
         )
 
-def make_audio_obj(project_id):
-    for j in Text.objects.filter(project=Project.objects.get(id=project_id)):
-        Audio.objects.create(
-            text=j,
-            savepoint=SavePoint.objects.get(project=Project.objects.get(id=project_id))
-        )
-
 def create_text_to_audio(project_id):
-    audio_list = Audio.objects.filter(text__project__id=project_id)
-    path=audio_list[0].savepoint.savepoint
+    text_list = Text.objects.filter(project__id=project_id).order_by("index")
+    print(text_list)
+    path=text_list[0].project.savedpoint
+    print(path)
     os.makedirs(path)
-    for audio in audio_list:
-        real_audio = gTTS(text=audio.text.text, lang="ko", slow=audio.speed)
+    for audio in text_list:
+        real_audio = gTTS(text=audio.text, lang="ko", slow=audio.speed)
         real_audio.save(path+f"{audio.id}.mp3")
--- api/views.py
+++ api/views.py
@@ -4,7 +4,7 @@ from rest_framework import status
 from rest_framework.decorators import parser_classes
 from rest_framework.parsers import JSONParser
 from decorators.execption_handler import execption_hanlder
-from api.utils.utils import preprocess_data, make_project_savepoint_obj, make_text_obj, make_audio_obj, create_text_to_audio
+from api.utils.utils import preprocess_data, make_project_obj, make_text_obj,  create_text_to_audio
 from django.db import transaction
 from api.service import AiParkService
 
@@ -23,11 +23,10 @@ class AiParkAPI(APIView):
 def create(request, *args, **kwargs):
     datas = request.data
     complete_preprocess = preprocess_data(datas["data"])
-    project_id = make_project_savepoint_obj()
+    project_id = make_project_obj()
     make_text_obj(project_id,complete_preprocess)
-    make_audio_obj(project_id)    
-    create_text_to_audio(project_id)
-    return JsonResponse({"result":"Success"}, status=status.HTTP_201_CREATED)
+    create_text_to_audio(project_id) 
+    return JsonResponse(status=status.HTTP_201_CREATED)
 
 
 @execption_hanlder()
@@ -35,4 +34,4 @@ def create(request, *args, **kwargs):
 def get_list(request, *args, **kwargs):
     project_id = kwargs.get("project_id")
     page = request.GET.get("page", 1)
-    return JsonResponse(aipark_service.get_list(project_id,page), safe=False)
+    return JsonResponse(aipark_service.get_list(project_id, page), safe=False)
