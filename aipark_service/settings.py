b4e7b4d2bc596334cb9d3b52513543153902d411
kdylsky <kdy9kdy@gmail.com>
2022-11-15 17:15:13 +0900

    [ADD] user 앱 추가

--- aipark_service/settings.py
+++ aipark_service/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    'api'
+    'api',
+    'users',
 ]
 
 MIDDLEWARE = [
--- api/models.py
+++ api/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from users.models import User
 
 class Project(models.Model):
-    # index = models.IntegerField()
-    # project_id = models.IntegerField()
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        db_column="user_id"
+    )
     project_title = models.CharField(max_length=50)
     created_time = models.DateTimeField(auto_now_add=True)
     updated_time = models.DateTimeField(auto_now=True)
